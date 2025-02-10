25eafc95db0863e01001d28c35f344aa3ee7e0ac
RavusiNbO <ravaravus@vk.com>
2025-02-10 15:01:40 +0300

    Ъ

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
-
+p_name = "category/<slug:category_slug>/"
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<int:pk>/", views.post_detail, name="post_detail"),
-    path("category/<slug:category_slug>/", 
-         views.category_posts, 
-         name="category_posts"),
+    path(p_name, views.category_posts, name="category_posts"),
 ]
